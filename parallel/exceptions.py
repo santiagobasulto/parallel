dc70db55b89301bccdb05e244c6a612491800328
Santiago Basulto <santiago.basulto@gmail.com>
2020-02-14 12:14:52 -0300

    Other files

--- parallel/exceptions.py
+++ parallel/exceptions.py
@@ -1,6 +1,11 @@
 class BaseParallelException(Exception):
     pass
 
+
 class TimeoutException(BaseParallelException):
     def __eq__(self, other):
         return type(self) == type(other)
+
+
+class ParallelStatusException(BaseParallelException):
+    pass
